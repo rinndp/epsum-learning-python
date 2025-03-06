1f36da8266cd0d7b443430df707264ca2931b195
axelPortatil <axelperez221105@gmail.com>
2025-03-06 18:02:27 +0100

    avance loginRegister

--- login/register.py
+++ login/register.py
@@ -6,26 +6,28 @@ print("---------------------")
 print("CREAR USUARIO")
 print("---------------------")
 
-if (not os.path.exists("users-data")):
+if not os.path.exists("users-data"):
     crearArchivo = open("users-data", "x")
+    with open("users-data", "w") as file:
+        file.write(json.dumps({"listaDeUsuarios": []}, indent=4))
 
-with open('users-data') as f:
+with open('users-data', "r") as f:
     json_cargado = json.load(f)
     print(json_cargado)
 
 usuario_unico = False
-while usuario_unico == False:
+while not usuario_unico:
     usuario_unico = True
     usuario_ingresado = input("Introduzca su usuario:\n")
-    for user in json_cargado:
-        print(user)
-
-
+    for user in json_cargado["listaDeUsuarios"]:
+        print(user["usuario"])
+        if user["usuario"] == usuario_ingresado:
+            usuario_unico = False
 
 password = input("Introduzca su passsword:\n")
 passwordHash = hashlib.sha256(password.encode()).hexdigest()
-with open("users-data", "a") as file:
-    file.write(json.dumps(
-        {"usuario": usuario_ingresado,
-         "password": passwordHash
-         }, indent=4))
+
+json_cargado["listaDeUsuarios"].append({"usuario": usuario_ingresado, "password": passwordHash})
+print(json_cargado)
+with open("users-data", "w") as file:
+    file.write(json.dumps(json_cargado, indent=4))
